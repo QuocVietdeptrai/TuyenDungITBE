8acc0eb5502f5f7c3c29a1a654a430cf6ebe9f73
QuocVietdeptrai <nguyenquocviet2004tb12@gmail.com>
2025-09-22 00:03:54 +0700

    Done search

--- routes/index.route.ts
+++ routes/index.route.ts
@@ -4,6 +4,7 @@ import authRouter from "./auth.route"
 import companyRouter from "./company.route"
 import cityRouter from "./city.route"
 import uploadRouter from "./upload.route"
+import searchRouter from "./search.route"
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use('/auth', authRouter);
 router.use('/company', companyRouter);
 router.use('/city', cityRouter);
 router.use('/upload', uploadRouter);
+router.use('/search', searchRouter);
 
 export default router;
